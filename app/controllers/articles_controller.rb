f59c5b3fec41ffb0a8ae6ed2c79c387e28db79eb
ashish <ashish92skumar@gmail.com>
2023-07-24 16:00:15 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+    render json: @articles
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    @article.update(article_params)
+      
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,44 @@
+class CommentsController < ApplicationController
+  def index 
+    @article = Article.find(params[:article_id])
+    render json: @article.comments
+  end
+
+  def show
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    render json: @comment
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.error
+    end
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.error
+    end
+  end
+
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
